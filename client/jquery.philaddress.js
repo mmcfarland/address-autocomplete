22c5a3aa4b84f614a24380a1a6134a60c760146d
Matthew McFarland <mmcfarland@gmail.com>
2013-04-22 22:01:18 -0400

    Use the actual server name
    After all this time.  This.
    
    Signed-off-by: Matthew McFarland <mmcfarland@gmail.com>

--- client/jquery.philaddress.js
+++ client/jquery.philaddress.js
@@ -1,7 +1,7 @@
 (function ($) {
     $.fn.philaddress = function (options) {
         var settings = $.extend({
-            url: 'ws://suggest.phillygrown.org',
+            url: 'ws://suggest.phillygrown.org/suggest/',
             minToSend: 3,
             maxResults: 10,
             onError: null,
